9b3a268470b854185301f2607b17ed25e80446ae
Rody <aimpugn@gmail.com>
2023-02-13 01:22:36 +0900

    learn memory virtualization

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
+#![allow(dead_code, unused_imports)]
 // here `src/main.rs` is crate root of binary
 
 // tell compiler to include the code it finds in `src/chapters.rs`
 pub mod chapters;
 pub mod util;
 
-use util::args::{ArgsMap, Config};
-
 // use public modules from `src/chapters/introduction/mod.rs`
-use crate::chapters::introduction::cpu;
+use chapters::introduction::*;
+use util::args::{ArgsMap, Config};
 
 fn main() {
     let _args = Config.parse();
-    cpu::print_and_sleep(_args.get("--param").unwrap().to_string());
+    // cpu::print_and_sleep(_args.get("--param").unwrap().to_string());
+    memory::alloc_then_print_and_sleep();
 }
